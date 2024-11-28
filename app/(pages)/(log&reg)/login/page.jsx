6faed970b6f4ab39cc4d1fd65785fb86cb6597bf
Artem <markelovartemka@icloud.com>
2024-11-27 23:36:17 -0700

    Clened up login sections -> should create components instead?

--- app/(pages)/(log&reg)/login/page.jsx
+++ app/(pages)/(log&reg)/login/page.jsx
@@ -43,47 +43,61 @@ export default function Login() {
         })
     }
 
+    const forgotPasswordWindow = () => {
+        return (
+            <div className="reset-password-wrapper">
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input value={email} onChange={(e) => {setEmail(e.target.value)}} id="email" name="email" type="email"></input>
+                </div>
+                <div className="password-info reset-password">
+                    <button className=" log_reg-button" onClick={()=> {sendResetPassword()}} >Send reset password link</button>
+                    <br></br>
+                    <a onClick={()=> {setForgotPassword(!forgotPassword)}} className="password-restore-link">Come back to log in</a>
+                </div>
+            </div>
+ 
+        )
+    }
+
     return (
         <div className="reg-main">
             <div className="form-container">
                 <div className="main-logo-login">
                     <img src="/images/logo_transparent.png" alt="logo" />
                 </div>
-                <form method="POST">
+                {forgotPassword ?
+                    <form className="initial-login" method="POST">
                     <div>
                         <label htmlFor="email">Email:</label>
                         <input value={email} onChange={(e) => {setEmail(e.target.value)}} id="email" name="email" type="email"></input>
                     </div>
                     {errorCode === errorInvalidEmail ? <div className="error-msg">Invalid Email</div> : ""}
-                    {forgotPassword ? 
-                    "" :
                     <div>
                         <label htmlFor="password">Password:</label>
                         <input required value={password} onChange={(e)=>{setPassword(e.target.value)}} id="password" name="password" type="password"></input>
-                    </div>}
+                    </div>
                     {(errorCode === errorMissingPswd || errorCode === errorInvalidPswd) ? <div className="error-msg">Invalid Password</div> : ""}
-                    {forgotPassword ? 
-                    <div className="password-info">
-                        <a onClick={()=> {sendResetPassword()}} className="password-restore-link">Send reset password link</a>
-                        <br></br>
-                        <a onClick={()=> {setForgotPassword(!forgotPassword)}} className="password-restore-link">Log in</a>
-                    </div>:
                     <div className="password-info">
                         <a onClick={()=> {setForgotPassword(!forgotPassword)}} className="password-restore-link">Forgot password?</a>
                     </div>
-                    }
-
                     <div className="login-info">
-                        <button className="log_reg-button" onClick={()=>{handleLogin()}} type="button"> Log in</button>
-                        <div className="register-link">New here? 
-                                        <Link
-                                            href="/register"
-                                            className="link">
-                                            Create account
-                                        </Link>
-                        </div>
+                    <button className="log_reg-button" onClick={()=>{handleLogin()}} type="button"> Log in</button>
+                    <div className="register-link">New here? 
+                                    <Link
+                                        href="/register"
+                                        className="link">
+                                        Create account
+                                    </Link>
                     </div>
-                </form>
+                    </div>
+
+                    </form>:
+                    <form className="forgot-password-window" method="POST">
+                        {forgotPasswordWindow()}
+                    </form>
+                } 
+
             </div>
             <div className="background-img-login">
             </div>
