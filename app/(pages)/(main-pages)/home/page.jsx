3fbdbf0332dc752167e742601b238e27465ee252
Artem <markelovartemka@icloud.com>
2025-02-14 14:57:42 -0700

    Fetched data

--- app/(pages)/(main-pages)/home/page.jsx
+++ app/(pages)/(main-pages)/home/page.jsx
@@ -1,18 +1,43 @@
 "use client"
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
+import { auth, app } from "../../../firebase/config";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+import LoadingPage from "../../../components/LoadingPage";
 
 export default function Home() {
 
-    const [students, setStudents] = useState([{name:"Artem"}, {name:"Elena"}]);
-    const [lessons, setLessons] = useState([{title:"Lesson with Nikolai"}, {title: "Lesson with Sasha"}]);
+    const [students, setStudents] = useState([]);
+    const [lessons, setLessons] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     //TEMPORARY
     const [dammyState, setDammyState] = useState(false);
 
-    useEffect((students) => {
+    useEffect(() => {
         setDammyState(!dammyState);
-    }, [students])
-    //
+        fetchData();
+    }, [students, lessons])
+
+    const userUid = auth.currentUser.uid;
+    const db = getFirestore(app);
+
+    const fetchData = async() => {
+        const collectionOfLessons = collection(db, 'users', userUid, 'lessons');
+        const collectionOfStudents = collection(db, 'users', userUid, 'students');
+        let getLessonsFromFirestore = [];
+        let getStudentsFromFirestore = [];
+        const docsSnapLessons = await getDocs(collectionOfLessons);   
+        const docsSnapStudents = await getDocs(collectionOfStudents);   
+        docsSnapLessons.forEach((doc) => {
+            getLessonsFromFirestore.push({...doc.data(), id: doc.id})
+        });
+        docsSnapStudents.forEach((doc) => {
+            getStudentsFromFirestore.push({...doc.data(), id: doc.id})
+        });
+        setLessons(getLessonsFromFirestore);
+        setStudents(getStudentsFromFirestore);
+        setLoading(false);
+    }
 
     return (
         <>
@@ -27,15 +52,19 @@ export default function Home() {
                 <div className="dashboard-block dash-students">
                     <div className="dash-block-title">Students</div>
                     <div className="dash-block-main">
-                        {(students.length > 0) ? 
-                        <div className="data">
-                            {students.map((student) => {
-                                return (<div className="dash-block-data">{student.name}</div>);
-                            })}
-                        </div>:
+                        {loading ? "Loading students..." : 
                         <>
-                            <div className="empty-msg">No students yet...</div>
-                            <div className="add-data">+</div>
+                            {(students.length > 0) ? 
+                                <div className="data">
+                                    {students.map((student) => {
+                                        return (<div className="dash-block-data">{student.name}</div>);
+                                    })}
+                                </div>:
+                                <>
+                                    <div className="empty-msg">No students yet...</div>
+                                    <div className="add-data">+</div>
+                                </>
+                            }
                         </>
                         }
                     </div>
@@ -43,17 +72,21 @@ export default function Home() {
                 <div className="dashboard-block dash-lessons-history">
                     <div className="dash-block-title">Lesson History</div>
                     <div className="dash-block-main">
-                    {(lessons.length > 0) ? 
-                        <div className="data">
-                            {lessons.map((lesson) => {
-                                return (<div className="dash-block-data">{lesson.title}</div>);
-                            })}
-                        </div>:
-                        <>
-                            <div className="empty-msg">No lessons history yet...</div>
-                            <div className="add-data">+</div>
-                        </>
+                    {loading ? "Loading lessons history..." : 
+                    <>
+                        {(lessons.length > 0) ? 
+                            <div className="data">
+                                {lessons.map((lesson) => {
+                                    return (<div className="dash-block-data">{lesson.title}</div>);
+                                })}
+                            </div>:
+                            <>
+                                <div className="empty-msg">No lessons history yet...</div>
+                                <div className="add-data">+</div>
+                            </>
                         }
+                    </>
+                    }
                     </div>
                 </div>
             </div>
